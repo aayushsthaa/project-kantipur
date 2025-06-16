fe108b2cdd38e4809fa90b8055798d4aa76d6185
aayushsthaa <91840826+aayushsthaa@users.noreply.github.com>
2025-06-16 23:24:04 +0545

    3rd day progress
    Completed the home page Ui and some basic javascript

---
+++ js/home.js
@@ -0,0 +1,30 @@
+let prevScrollPos = window.pageYOffset;
+let navBottom=document.querySelector(".navigation-bottom");
+let menu = document.querySelector(".menu-hamburger");
+window.addEventListener('scroll',scrollEvent);
+function scrollEvent() {
+  const currentScrollPos = window.pageYOffset;
+  const maxHeight = document.documentElement.scrollHeight;
+  if (currentScrollPos > prevScrollPos) {
+      if (window.scrollY + window.innerHeight >= maxHeight) {
+        navBottom.classList.remove("hidden");
+  }else{
+        navBottom.classList.add("hidden");
+  }
+  } else {
+    navBottom.classList.remove("hidden");
+  }
+  prevScrollPos = currentScrollPos;
+}
+
+function menuFunction(){
+    if(menu.classList.contains('hamburger-active')){
+        menu.classList.remove('hamburger-active');
+        menu.classList.add('menu-hamburger');
+    }else{
+        menu.classList.remove('menu-hamburger');
+        menu.classList.add("hamburger-active");
+    }
+}
+
+menu.addEventListener("click", menuFunction);
